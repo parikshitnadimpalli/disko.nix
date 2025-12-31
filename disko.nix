9f243be4fb1221ef0c846db9e434a2759fdfffd6
Parikshit Nadimpalli <109894397+parikshitnadimpalli@users.noreply.github.com>
2025-12-31 17:37:39 +0530

    First upload
    Literally the first upload

---
+++ disko.nix
@@ -0,0 +1,50 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G"; # Adjust based on how many kernel images you want
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ]; # To ensure only root user can read/write
+              };
+            };
+            Swap = {
+              size = "35G"; # Adjust based on installed RAM
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # Enables Hibernation
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "~/secret.key";
+                };
+                content = {
+                  type = "filesystem";
+                  format = "btrfs";
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" "noatime" ]; # Disk compression and writing access time prevention
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
